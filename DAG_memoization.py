5af847b8be9c434a0ec7054e0a21205d7ab96132
Ali Tamer Ünal <atu@icrontech.com>
2022-04-06 11:36:16 +0300

    Added dynamic programming formulation with memoization.

--- DAG_memoization.py
+++ DAG_memoization.py
@@ -1,9 +1,13 @@
 
 # not object oriented!!
 
+from re import X
+
+
 class Node:
     def __init__(self, code: str) -> None:
         self.code = code
+        self.cache = {}
         
 class DAG:
     def __init__(self) -> None:
@@ -32,7 +36,18 @@ class DAG:
 
         min = 0
         for a in self.incomming_arcs(v):
-            length = a.distance + self.SP(s, a.from_node)
+            
+            # check the cache if we had already calculated the distance from the start to the begining of arc a.
+            # this is called 'memoization'. note that we only recurse if we cannot access the distance from the cache.
+
+            distance_from_start_to_from_node_of_a = 0
+            if (s, a.from_node) in self.cache.keys():
+                distance_from_start_to_from_node_of_a = self.cache[(s, a.from_node)]
+            else:
+                distance_from_start_to_from_node_of_a = self.SP(s, a.from_node)
+                self.cache[(s, a.from_node)] = distance_from_start_to_from_node_of_a
+
+            length = a.distance + distance_from_start_to_from_node_of_a
             if (min <= 0 or length < min):
                 min = length
 
